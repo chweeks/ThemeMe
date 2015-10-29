7ecb334922ce47169955eb2a40e70e99a333b458
chweeks <chrisharryweeks@gmail.com>
2015-10-29 15:39:15 +0000

    updated tests

--- test/e2e/feature.js
+++ test/e2e/feature.js
@@ -7,9 +7,12 @@ describe("themeMe", function() {
   var playRandom = element(by.id('playRandom'));
   var searchTerm = element(by.model('searchTerm'));
   var searchButton = element(by.id('searchSC'));
+  var playHeader = element(by.id('playHeader'));
+  var username = element(by.id('username'));
+  var comment = element(by.id('comment'));
 
-  describe("On startup", function(){
-    beforeEach(function(){
+  describe("On startup", function() {
+    beforeEach(function() {
       browser.get('http://localhost:8080/www/index.html');
     });
 
@@ -17,50 +20,79 @@ describe("themeMe", function() {
       expect(browser.getTitle()).toEqual('Theme Me');
     });
 
-    it('has a name field', function(){
+    it('has a name field', function() {
       expect(name.isDisplayed()).toBeTrue;
     });
 
-    it('has a message field', function(){
+    it('has a message field', function() {
       expect(message.isDisplayed()).toBeTrue;
     });
 
-    it('has a choose song button', function(){
+    it('has a choose song button', function() {
       expect(chooseSong.isDisplayed()).toBeTrue;
     });
 
-    it('has a play random song button', function(){
+    it('has a play random song button', function() {
       expect(playRandom.isDisplayed()).toBeTrue;
     });
 
-    it('name field is empty', function(){
+    it('name field is empty', function() {
       expect(name).toBeUndefined;
     });
 
-    it('message field is empty', function(){
+    it('message field is empty', function() {
       expect(message).toBeUndefined;
     });
   });
 
   describe('when name and message are input', function() {
 
-    beforeEach(function(){
+    beforeEach(function() {
       browser.get('http://localhost:8080/www/index.html');
       name.sendKeys('test');
       message.sendKeys('testy testy');
       chooseSong.click();
     });
 
-    it('has a search bar', function(){
+    it('redirects you to the set song page', function() {
+      expect(browser.getCurrentUrl()).toEqual('http://localhost:8080/www/index.html#/setsong')
+    });
+
+    it('has a search bar', function() {
       expect(searchTerm.isDisplayed()).toBeTrue;
     });
 
-    it('search bar is empty', function(){
+    it('search bar is empty', function() {
       expect(searchTerm).toBeUndefined;
     });
 
-    it('has a search button', function(){
+    it('has a search button', function() {
       expect(searchButton.isDisplayed()).toBeTrue;
+    });
+
+  });
+
+  describe('when random song button is pressed', function() {
+
+    beforeEach(function(){
+      browser.get('http://localhost:8080/www/index.html');
+      playRandom.click();
+    });
+
+    it('redirects you to the play page', function() {
+      expect(browser.getCurrentUrl()).toEqual('http://localhost:8080/www/index.html#/playtheme')
+    });
+
+    it('has friends unite header', function() {
+      expect(playHeader.getText()).toContain('Friends')
+    });
+
+    it('has friends unite header', function() {
+      expect(username.isDisplayed()).toBeTrue;
+    });
+
+    it('has friends unite header', function() {
+      expect(comment.isDisplayed()).toBeTrue;
     });
 
   });
